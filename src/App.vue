d111021e50f78e99c8a53bb4e4cedee707717ca0
Jesray Garciano <jesray.garciano@gmail.com>
2018-10-02 10:53:38 +0800

    Adding Page and Notebook components and modified their stylesheets

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,58 @@
-<template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
-  </div>
-</template>
+    <template>
+      <div id="app">
+          <Notebook @change-page="changePage" @new-page="newPage" :pages="pages" :activePage="index" />
+          <Page @save-page="savePage" @delete-page="deletePage" :page="pages[index]" />
+      </div>
+    </template>
 
-<script>
-import HelloWorld from './components/HelloWorld'
+    <script>
+    import Notebook from './components/Notebook'
+    import Page from './components/Page'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+    export default {
+      name: 'app',
+      components: {
+        Notebook,
+        Page
+      },
+      data: () => ({
+        pages: [],
+        index: 0
+      }),
+      methods: {
+        newPage () {
+          this.pages.push({
+            title: '',
+            content: ''
+          })
+          this.index = this.pages.length - 1
+        },
+        changePage (index) {
+          this.index = index
+        },
+        savePage () {
+           // nothing as of yet
+        },
+        deletePage () {
+          this.pages.splice(this.index, 1)
+          this.index = Math.max(this.index - 1, 0)
+        }
+      }
+    }
+    </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+    <style>
+    html, body, #app {
+        height: 100%;
+    }
+
+    body {
+        margin: 0;
+    }
+
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        display: flex;
+        flex-direction: row;
+    }
+    </style>
